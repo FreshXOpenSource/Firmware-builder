03ac7a1b2dfb1f337b2e2da610e03506d2aa27b1
Kai Mosebach <mokai@urz-n-mba.unibas.ch>
2016-01-11 18:16:07 +0100

    zbar w/ video support

--- formula/libc.sh
+++ formula/libc.sh
@@ -9,19 +9,33 @@ GV_depend=()
 FU_tools_get_names_from_url
 FU_binaries_installed "ldconfig"
 
+false
+
 if [ $? == 1 ]; then
 
 	#rsync -avp ${UV_toolchain_dir}/${UV_toolchain_libc}/lib/ ${UV_sysroot_dir}/lib/
-	rsync -avp ${UV_toolchain_dir}/${UV_toolchain_libc}/${UV_target}/libc/lib/ ${UV_sysroot_dir}/lib/
+	if [ -d ${UV_toolchain_dir}/${UV_target}/libc/lib/ ]; then
+		SRC=${UV_toolchain_dir}/${UV_target}/libc/lib
+		GCCSRC=${UV_toolchain_dir}/${UV_target}
+	else
+	    SRC=${UV_toolchain_dir}/${UV_toolchain_libc}/${UV_target}/libc/lib
+		GCCSRC=${UV_toolchain_dir}/${UV_target}/libc
+	fi
+
+	echo $SRC
+
+	rsync -avp $SRC/* ${UV_sysroot_dir}/lib/
 
 	if [ $? == 1 ]; then
 		exit 1
 	fi
-	rsync -avp ${UV_toolchain_dir}/${UV_target}/lib/libgcc_s.so* ${UV_sysroot_dir}/lib/
+	rsync -avp $GCCSRC/lib/libgcc_s.so* ${UV_sysroot_dir}/lib/
 	if [ $? == 1 ]; then
 		exit 1
 	fi
-	rsync -avp ${UV_toolchain_dir}/${UV_toolchain_libc}/${UV_target}/libc/sbin/ldconfig ${UV_sysroot_dir}/usr/bin/
+
+	rsync -avp $GCCSRC/libc/sbin/ldconfig ${UV_sysroot_dir}/usr/bin/
+
 	if [ $? == 1 ]; then
 		exit 1
 	fi
--- formula/zbar.sh
+++ formula/zbar.sh
@@ -5,8 +5,8 @@ GV_sha1="a389e21eb6b796689c6a2379e603b13bbf6d58c7"
 
 FU_tools_get_names_from_url
 GV_tar_name="zbar-0.10.tar.gz"
-GV_version="0.10"
 GV_dir_name="zbar-0.10"
+GV_version="0.10"
 FU_tools_installed "zbar.pc"
 
 if [ $? == 1 ]; then
@@ -27,12 +27,18 @@ if [ $? == 1 ]; then
 		"--without-gtk"
 		"--without-qt"
 		"--without-x"
-		"--disable-video"
+#		"--disable-video"
 	)
 
 	FU_file_get_download
 	FU_file_extract_tar
-	
+
+	gsed -i "s/linux\/videodev.h/libv4l1-videodev.h/" ${GV_source_dir}/${GV_dir_name}/configure.ac
+	gsed -i "s/linux\/videodev.h/libv4l1-videodev.h/" ${GV_source_dir}/${GV_dir_name}/configure
+	gsed -i "s/linux\/videodev.h/libv4l1-videodev.h/" ${GV_source_dir}/${GV_dir_name}/zbar/video/v4l1.c
+	#rm ${GV_source_dir}/${GV_dir_name}/configure
+	#(cd ${GV_source_dir}/${GV_dir_name}; autoreconf -f)
+
 	FU_build_configure
 	FU_build_make
 	FU_build_install
