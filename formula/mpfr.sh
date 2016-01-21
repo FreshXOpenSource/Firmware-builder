c0c4109dc2deee87d68c8612e5d136dcd2dec1ed
Kai Mosebach <kai@freshx.de>
2016-01-21 10:18:34 +0100

    gcc-base nonfunctional

--- formula/mpfr.sh
+++ formula/mpfr.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-GV_url="http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.bz2"
-GV_sha1="46d5a11a59a4e31f74f73dd70c5d57a59de2d0b4"
-
-GV_depend=()
+GV_url="http://www.mpfr.org/mpfr-current/mpfr-3.1.3.tar.xz"
+GV_sha1="383303f9de5ebe055b03b94642b03465baf9e6c7"
 
 FU_tools_get_names_from_url
-FU_tools_installed "${LV_formula%;*}.pc"
 
 if [ $? == 1 ]; then
 	
@@ -15,24 +12,18 @@ if [ $? == 1 ]; then
 	GV_args=(
 		"--host=${GV_host}"
 		"--prefix=${GV_prefix}" 
-		
 		"--libdir=${UV_sysroot_dir}/lib"
 		"--includedir=${UV_sysroot_dir}/include"
 		"--enable-shared"
-		"--disable-static"
-		"--enable-gmp-internals"
-		"--enable-assert"
+
 	)
 	
 	FU_file_get_download
 	FU_file_extract_tar
-		
-	FU_build_configure	
-	FU_build_make
-	FU_build_install "install-strip"
 	
-	PKG_libs="-lmpfr"
-	
-	FU_build_pkg_file
+	FU_build_configure
+	FU_build_make	
+	FU_build_install
+
 	FU_build_finishinstall
 fi
