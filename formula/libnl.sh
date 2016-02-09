c7a1a8450ddd72ddcd3408197f538ecd26bee648
Kai Mosebach <kai@freshx.de>
2016-02-09 18:14:11 +0100

    formula fixes

--- formula/libnl.sh
+++ formula/libnl.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+#
+#	requires a newer bison (>=3.0)
+#
+
 GV_url="http://www.infradead.org/~tgr/libnl/files/libnl-3.2.25.tar.gz"
 GV_sha1="b7a4981f7edf7398256d35fd3c0b87bc84ae27d1"
 
 GV_depend=()
 
 FU_tools_get_names_from_url
-FU_tools_installed "${LV_formula%;*}.pc"
+FU_tools_installed "libnl-3.0.pc"
 
 if [ $? == 1 ]; then
 	
 	FU_tools_check_depend
 
 	GV_args=(
+		"--host=${UV_target}"
 		"--prefix=${UV_sysroot_dir}" 
 		"--libdir=${UV_sysroot_dir}/lib"
 		"--bindir=${UV_sysroot_dir}/bin"
 		"--includedir=${UV_sysroot_dir}/include"
+		"--disable-cli"
 	)
 
 	FU_file_get_download
--- formula/linux-header.sh
+++ formula/linux-header.sh
@@ -5,12 +5,13 @@ GV_sha1="683398e7ae5b72f3fb71910f58728938cc2b6dc5"
 
 FU_tools_get_names_from_url
 
-if [ $? == 1 ]; then
 	
-	if [ ! -e ${UV_sysroot_dir}/include/Kbuild ]; then
+if [ ! -e ${UV_sysroot_dir}/opt/kernel/include/Kbuild ]; then
 		FU_file_get_download
-		FU_file_extract_tar
+		FU_file_extract_tar --include include/*
+		mkdir -p ${UV_sysroot_dir}/opt/kernel/include
 		cp -rp ${GV_source_dir}/${GV_dir_name}/include/* ${UV_sysroot_dir}/opt/kernel/include
-	fi
-
+		for i in `find ${UV_toolchain_dir}/${UV_target} -name *.h | sed "s/.*usr\/include\///"`; do
+			test -f ${UV_sysroot_dir}/opt/kernel/include/$i && rm ${UV_sysroot_dir}/opt/kernel/include/$i
+		done
 fi
